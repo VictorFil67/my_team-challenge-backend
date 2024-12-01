857ba3195298fc806b36e99bcd1c3006ba030119
VictorFil67 <victorfilinovych@gmail.com>
2024-12-01 19:08:14 +0200

    created sendEmail helper

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,22 +11,24 @@ const { JWT_SECRET } = process.env;
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
-  console.log(`*${password}*`);
+
   const user = await User.findOne({ email });
   if (user) {
     throw HttpError(409, "This email is already in use");
   }
+
   const newUser = await register(req.body);
+
   const payload = { id: newUser._id };
-  console.log("payload: ", newUser._id);
   const accessToken = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
-  console.log(accessToken);
+
   const refreshToken = jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
-  console.log(refreshToken);
 
   const tokens = await setTokens(newUser._id, accessToken, refreshToken);
   console.log(tokens);
+
   const loggedInUser = await User.findById({ _id: newUser._id }, "-password");
+
   res.status(201).json({
     loggedInUser,
   });
