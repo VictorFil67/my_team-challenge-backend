78b0522b09dbadf80a76c05d6e0743b45bd48fba
VictorFil67 <victorfilinovych@gmail.com>
2024-12-01 13:09:38 +0200

    added register controller

---
+++ controllers/authController.js
@@ -0,0 +1,23 @@
+import User from "../models/User.js";
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  // console.log(`*${password}*`);
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "This email is already in use");
+  }
+  const newUser = await register(req.body);
+  const payload = { id: newUser._id };
+  console.log("payload: ", newUser._id);
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  console.log(token);
+  const refreshToken = jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
+  console.log(refreshToken);
+  const tokens = await setTokens(newUser._id, token, refreshToken);
+  console.log(tokens);
+  const loggedInUser = await User.findById({ _id: newUser._id }, "-password");
+  res.status(201).json({
+    loggedInUser,
+  });
+};
