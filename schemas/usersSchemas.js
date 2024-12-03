f8e4348abab8b72fd76d99847c74335bdf36a573
VictorFil67 <victorfilinovych@gmail.com>
2024-12-03 21:38:50 +0200

    added signin joi schema to userShemas

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -12,7 +12,7 @@ export const signupSchema = Joi.object({
           "The password may contain at least one upper case, one lower case English letter, one digit, and have a length of at least 8 characters"
         )
     ),
-  phone: [Joi.string(), Joi.number()]
+  phone: Joi.string()
     .pattern(
       new RegExp(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
     )
@@ -22,6 +22,19 @@ export const signupSchema = Joi.object({
       (errors) =>
         new Error(
           "Must be from 10 to 12 digits (+111222333444) with or without '+'"
+        )
+    ),
+});
+
+export const signinSchema = Joi.object({
+  email: Joi.string().email().required().trim(),
+  password: Joi.string()
+    .pattern(new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/))
+    .required()
+    .error(
+      (errors) =>
+        new Error(
+          "The password may contain at least one upper case, one lower case English letter, one digit, and have a length of at least 8 characters"
         )
     ),
 });
