badb727ff6170a47eab4cfe0727f97494971ca7a
VictorFil67 <victorfilinovych@gmail.com>
2024-12-03 22:15:31 +0200

    created updateUser service

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,1 +1,10 @@
-const addUserAddresses = async (req, res) => {};
+import ctrlWrapper from "../decorators/ctrlWrapper.js";
+import { updateUser } from "../services/userServices.js";
+
+const addUserAddresses = async (req, res) => {
+  const { _id } = req.user;
+  const result = await updateUser(_id, req.body, "-password");
+  res.status(200).json(result);
+};
+
+export default { addUserAddresses: ctrlWrapper(addUserAddresses) };
--- services/userServices.js
+++ services/userServices.js
@@ -3,3 +3,7 @@ import User from "../models/User.js";
 export function findUser(filter) {
   return User.findOne(filter);
 }
+
+export function updateUser(id, data) {
+  return User.findByIdAndUpdate(id, data, { new: true });
+}
