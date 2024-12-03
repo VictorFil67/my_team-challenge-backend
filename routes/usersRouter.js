40a5825fb44e32a21965b9680282dc8ab5c11cbe
VictorFil67 <victorfilinovych@gmail.com>
2024-12-03 22:38:39 +0200

    created add addresses to user route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,13 @@
 import express from "express";
 import authenticate from "../middlewares/authenticate.js";
+import usersControllers from "../controllers/usersControllers.js";
 
 const usersRouter = express.Router();
 
 usersRouter.use(authenticate);
+
+const { addUserAddresses } = usersControllers;
+
+usersRouter.put("/addresses", addUserAddresses);
 
 export default usersRouter;
