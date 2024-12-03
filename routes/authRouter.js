b16069a97fa6eb8398c046d8b9e9c42b31120172
VictorFil67 <victorfilinovych@gmail.com>
2024-12-03 21:44:24 +0200

    added body validation to login route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,14 +2,14 @@ import express from "express";
 import authController from "../controllers/authControllers.js";
 import authenticate from "../middlewares/authenticate.js";
 import validateBody from "../decorators/validateBody.js";
-import { signupSchema } from "../schemas/usersSchemas.js";
+import { signinSchema, signupSchema } from "../schemas/usersSchemas.js";
 
 const authRouter = express.Router();
 
 const { signup, signin, logout } = authController;
 
 authRouter.post("/register", validateBody(signupSchema), signup);
-authRouter.post("/login", signin);
+authRouter.post("/login", validateBody(signinSchema), signin);
 authRouter.post("/logout", authenticate, logout);
 
 export default authRouter;
