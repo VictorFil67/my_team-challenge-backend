b231b8ac68a3842c47ec317267bf43cfda365a89
VictorFil67 <victorfilinovych@gmail.com>
2024-12-03 21:31:32 +0200

    added signupSchema validate to register route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 import authController from "../controllers/authControllers.js";
 import authenticate from "../middlewares/authenticate.js";
+import validateBody from "../decorators/validateBody.js";
+import { signupSchema } from "../schemas/usersSchemas.js";
 
 const authRouter = express.Router();
 
 const { signup, signin, logout } = authController;
 
-authRouter.post("/register", signup);
+authRouter.post("/register", validateBody(signupSchema), signup);
 authRouter.post("/login", signin);
 authRouter.post("/logout", authenticate, logout);
 
