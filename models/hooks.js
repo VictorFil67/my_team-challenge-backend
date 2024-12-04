bb9705e98977b5da00947141bede5d8b5ddd8378
VictorFil67 <victorfilinovych@gmail.com>
2024-12-04 08:56:38 +0200

    added setUpdateSetting hook for displaing up-to-date data when updating user

--- models/hooks.js
+++ models/hooks.js
@@ -3,3 +3,9 @@ const handleSaveError = (error, data, next) => {
   error.status = name === "MongoServerError" && code === 11000 ? 409 : 400;
   next();
 };
+
+export const setUpdateSetting = function (next) {
+  this.options.new = true;
+  this.options.runValidators = true;
+  next();
+};
