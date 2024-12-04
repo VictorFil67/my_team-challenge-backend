de6ecaa93ce2e567f99b9f2c66cbb3dab65ee2b9
VictorFil67 <victorfilinovych@gmail.com>
2024-12-04 20:47:23 +0200

    added duplicate checking for addUserAddresses controller

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,10 +1,26 @@
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
+import HttpError from "../helpers/HttpError.js";
 import { findUserById, updateUser } from "../services/userServices.js";
 
 const addUserAddresses = async (req, res) => {
   const { _id } = req.user;
+  const { residential_complex, building, entrance, apartment } = req.body;
   const { buildings } = await findUserById(_id);
   console.log(buildings);
+  const existedAddress = buildings.find(
+    (userAddress) =>
+      userAddress.residential_complex === residential_complex &&
+      userAddress.building === building &&
+      userAddress.entrance === entrance &&
+      userAddress.apartment === apartment
+  );
+  if (existedAddress) {
+    throw HttpError(
+      403,
+      `This address already exists, so you can't write down this address once more`
+      //   `You don't have access to this action, because you have already voted!`
+    );
+  }
   buildings.push({ ...req.body });
   console.log(buildings);
   const result = await updateUser(_id, { buildings });
