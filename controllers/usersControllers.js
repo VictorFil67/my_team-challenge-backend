a4b3509fd52fa7c6a71c9b132342481c23fe75a9
VictorFil67 <victorfilinovych@gmail.com>
2024-12-07 21:51:30 +0200

    added email notificication to addUserAddresses controller

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,9 +1,10 @@
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
 import HttpError from "../helpers/HttpError.js";
+import sendEmail from "../helpers/sendEmail.js";
 import { findUserById, updateUser } from "../services/userServices.js";
 
 const addUserAddresses = async (req, res) => {
-  const { _id } = req.user;
+  const { _id, email, name } = req.user;
   const { residential_complex, building, entrance, apartment } = req.body;
   const { buildings } = await findUserById(_id);
   //   console.log(buildings);
@@ -24,6 +25,18 @@ const addUserAddresses = async (req, res) => {
   buildings.push({ ...req.body });
   //   console.log(buildings);
   const result = await updateUser(_id, { buildings });
+  const userEmail = {
+    to: email,
+    subject: "Your addresses",
+    html: `<h1>Hello, ${name}!</h1>
+        <p>Congratulations! Your addresses have been successfully added to your profile.</p>
+         <p>Now you need to wait for the moderator's approval</p>
+         <p>You will be notified of this by email</p>
+         <p>If you have any questions, you can always contact our support team.</p>
+        <p style="margin-top: 10px;">Best regards,</p>
+        <p style="margin-top: 10px;">The Teamchallenge Chat Team</p>`,
+  };
+  await sendEmail(userEmail);
   res.status(200).json(result);
 };
 
