50f319241f08dcdda771c16adc5605707343c3cc
VictorFil67 <victorfilinovych@gmail.com>
2024-12-12 14:22:53 +0200

    added isValidId middleware

---
+++ middlewares/isValidId.js
@@ -0,0 +1,16 @@
+import { isValidObjectId } from "mongoose";
+import HttpError from "../helpers/HttpError.js";
+
+const isValidId = (req, res, next) => {
+  const keys = Object.keys(req.params);
+
+  keys.forEach((key) => {
+    if (!isValidObjectId(req.params[key])) {
+      throw HttpError(404, `${req.params[key]} is not valid id`);
+    }
+    console.log(req.params[key]);
+  });
+  next();
+};
+
+export default isValidId;
