fc6cdad9897c678e5af0e0b899696b9b23d4ceb5
VictorFil67 <victorfilinovych@gmail.com>
2024-12-16 20:11:01 +0200

    created updateComplexSchema for Joi validation

--- schemas/complexSchema.js
+++ schemas/complexSchema.js
@@ -3,7 +3,6 @@ import Joi from "joi";
 export const createComplexSchema = Joi.object({
   name: Joi.string().required().trim(),
   images: Joi.array().items(Joi.string()),
-  //   properties: Joi.object({
   parking: Joi.boolean().default(false),
   security: Joi.boolean().default(false),
   access_control: Joi.boolean().default(false),
@@ -13,6 +12,33 @@ export const createComplexSchema = Joi.object({
   video_surveillance: Joi.boolean().default(false),
   floors: Joi.number().default(1),
   entrances: Joi.number().default(1),
-  //   }),
   addresses: Joi.array().items(Joi.string().required().trim()),
+});
+
+export const updateComplexSchema = Joi.object({
+  name: Joi.string().trim(),
+  images: Joi.array().items(Joi.string()),
+  properties: Joi.object({
+    parking: Joi.boolean(),
+    security: Joi.boolean(),
+    access_control: Joi.boolean(),
+    concierge: Joi.boolean(),
+    playground: Joi.boolean(),
+    closed_area: Joi.boolean(),
+    video_surveillance: Joi.boolean(),
+    floors: Joi.number(),
+    entrances: Joi.number(),
+  }),
+  buildings: Joi.array().items(
+    Joi.object({
+      address: Joi.string(),
+      appartments: Joi.array().items(
+        Joi.object({
+          number: Joi.number(),
+          entrance: Joi.number(),
+          services_debt: Joi.number(),
+        })
+      ),
+    })
+  ),
 });
