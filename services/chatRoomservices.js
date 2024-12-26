531fed94f61f5e343aab33ff6ee8ae7390fe0170
VictorFil67 <victorfilinovych@gmail.com>
2024-12-27 00:23:59 +0200

    created getActiveChat controller

--- services/chatRoomservices.js
+++ services/chatRoomservices.js
@@ -3,3 +3,6 @@ import ChatRoom from "../models/ChatRoom.js";
 export async function getChatRooms(filter) {
   return await ChatRoom.find(filter);
 }
+export async function getChatRoom(filter) {
+  return await ChatRoom.findOne(filter);
+}
