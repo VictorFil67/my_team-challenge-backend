ef0724454155c45ca5f0fd7b87009324e5f0169e
VictorFil67 <victorfilinovych@gmail.com>
2024-12-26 23:35:07 +0200

    fullfiled getUserChatRooms request

--- controllers/chatRoomsControllers.js
+++ controllers/chatRoomsControllers.js
@@ -3,9 +3,10 @@ import { getChatRooms } from "../services/chatRoomservices.js";
 
 const getUserChatRooms = async (req, res) => {
   const { _id } = req.user;
+  console.log(_id);
   const result = await getChatRooms({
     users: {
-      $elemMatch: { _id },
+      $all: [_id], //The $all operator selects the documents where the value of a field is an array that contains all the specified elements.
     },
   });
   res.status(200).json(result);
