249f258ae63823544ef4a75f215910ad1f92f121
VictorFil67 <victorfilinovych@gmail.com>
2024-12-26 21:53:19 +0200

    created chatRoomsRouter

--- routes/chatRoomsRouter.js
+++ routes/chatRoomsRouter.js
@@ -1,10 +1,10 @@
-// import express from "express";
-// import chatRoomsControllers from "../controllers/chatRoomsControllers";
+import express from "express";
+import chatRoomsControllers from "../controllers/chatRoomsControllers";
 
-// const chatRoomsRouter = express.Router();
+const chatRoomsRouter = express.Router();
 
-// const { getUserChatRooms } = chatRoomsControllers;
+const { getUserChatRooms } = chatRoomsControllers;
 
-// chatRoomsRouter.post("/", getUserChatRooms);
+chatRoomsRouter.post("/", getUserChatRooms);
 
-// export default chatRoomsRouter;
+export default chatRoomsRouter;
