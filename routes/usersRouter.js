75ee40bcac4e1c4074dfde75e6c16b13d778da36
VictorFil67 <victorfilinovych@gmail.com>
2024-12-27 21:28:46 +0200

    completed creating or updating chat room request

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -4,6 +4,7 @@ import usersControllers from "../controllers/usersControllers.js";
 import validateBody from "../decorators/validateBody.js";
 import { addUserAddressesSchema } from "../schemas/usersSchemas.js";
 import isValidId from "../middlewares/isValidId.js";
+import chatRoomsControllers from "../controllers/chatRoomsControllers.js";
 
 const usersRouter = express.Router();
 
@@ -11,6 +12,7 @@ usersRouter.use(authenticate);
 
 const { addUserAddresses, deleteUserAddress, approveUserAddress } =
   usersControllers;
+const { createChatRooom } = chatRoomsControllers;
 
 usersRouter.put(
   "/addresses",
@@ -22,6 +24,11 @@ usersRouter.delete(
   validateBody(addUserAddressesSchema),
   deleteUserAddress
 );
-usersRouter.patch("/addresses/:userId", isValidId, approveUserAddress);
+usersRouter.patch(
+  "/addresses/:userId",
+  isValidId,
+  approveUserAddress,
+  createChatRooom
+);
 
 export default usersRouter;
--- services/chatRoomservices.js
+++ services/chatRoomservices.js
@@ -6,3 +6,11 @@ export async function getChatRooms(filter) {
 export async function getChatRoom(filter) {
   return await ChatRoom.findOne(filter);
 }
+
+export async function createRoom(data) {
+  return await ChatRoom.create(data);
+}
+
+export async function updateRoom(filter, data) {
+  return await ChatRoom.findOneAndUpdate(filter, data, { new: true });
+}
