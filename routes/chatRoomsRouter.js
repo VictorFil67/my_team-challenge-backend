7c0982f900c0d4fb3ab7060d411581627446c558
VictorFil67 <victorfilinovych@gmail.com>
2024-12-28 21:33:07 +0200

    completed createChatForTwo request

--- routes/chatRoomsRouter.js
+++ routes/chatRoomsRouter.js
@@ -1,14 +1,17 @@
 import express from "express";
 import chatRoomsControllers from "../controllers/chatRoomsControllers.js";
 import authenticate from "../middlewares/authenticate.js";
+import isValidId from "../middlewares/isValidId.js";
 
 const chatRoomsRouter = express.Router();
 
 chatRoomsRouter.use(authenticate);
 
-const { getUserChatRooms, getActiveChat } = chatRoomsControllers;
+const { getUserChatRooms, getActiveChat, createChatForTwo } =
+  chatRoomsControllers;
 
 chatRoomsRouter.get("/", getUserChatRooms);
-chatRoomsRouter.get("/:chatId", getActiveChat);
+chatRoomsRouter.get("/:chatId", isValidId, getActiveChat);
+chatRoomsRouter.post("/", createChatForTwo);
 
 export default chatRoomsRouter;
