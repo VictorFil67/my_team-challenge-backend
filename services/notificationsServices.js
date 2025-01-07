8399498945305d273b4f2c0f133512d83d49f68d
VictorFil67 <victorfilinovych@gmail.com>
2025-01-07 15:33:48 +0200

    created find notification service

--- services/notificationsServices.js
+++ services/notificationsServices.js
@@ -7,3 +7,7 @@ export async function addNotification(data) {
 export function listNotificationsByFilter(filter, query) {
   return Notification.find(filter, "", query).sort({ updatedAt: -1 });
 }
+
+export function findNotification(id) {
+  return Notification.findById(id);
+}
