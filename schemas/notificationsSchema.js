c5dff08a6f8697d57c2439fb642b6f6c3467c0a8
VictorFil67 <victorfilinovych@gmail.com>
2025-01-11 19:00:41 +0200

    added joi validation to update notification request

--- schemas/notificationsSchema.js
+++ schemas/notificationsSchema.js
@@ -6,7 +6,7 @@ export const notificationsSchema = Joi.object({
   building_id: Joi.string(),
 });
 
-export const notificationsUpdateSchema = Joi.object({
+export const updatenotificationsSchema = Joi.object({
   text: Joi.string(),
   type: Joi.string().valid("events", "emergency"),
 });
