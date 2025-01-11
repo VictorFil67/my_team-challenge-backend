083179f406e792dfca9aad5efff5cbee3829de31
VictorFil67 <victorfilinovych@gmail.com>
2025-01-11 18:39:36 +0200

    created notificationsUpdateSchema for validation request body

--- schemas/notificationsSchema.js
+++ schemas/notificationsSchema.js
@@ -5,3 +5,8 @@ export const notificationsSchema = Joi.object({
   type: Joi.string().valid("events", "emergency").required(),
   building_id: Joi.string(),
 });
+
+export const notificationsUpdateSchema = Joi.object({
+  text: Joi.string(),
+  type: Joi.string().valid("events", "emergency"),
+});
