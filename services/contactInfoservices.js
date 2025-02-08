14e66cd1d18a2ea68ebe94e5d7b1c94f833a5587
VictorFil67 <victorfilinovych@gmail.com>
2025-02-08 20:08:04 +0200

    created createContactInfo controller

--- services/contactInfoservices.js
+++ services/contactInfoservices.js
@@ -3,3 +3,7 @@ import ContactInfo from "../models/ContactInfo.js";
 export async function makeContactInfo(data) {
   return await ContactInfo.create(data);
 }
+
+export function findContactInfo(params) {
+  return ContactInfo.findOne(params);
+}
