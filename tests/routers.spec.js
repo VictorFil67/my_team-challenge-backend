c928c2f1e1f167577d3a20ce1c8f3c96ab183f15
avdiachenko <134641511+avdiachenko@users.noreply.github.com>
2025-02-09 14:31:34 +0200

    Add Unit Tests (#195)
    * update package.json
    
    * update package.json again
    
    * update package.json yet again
    
    * feature: tests for /auth
    
    * tests for auth, complexes, users routers
    
    * Pull changes for testing (#183)
    
    * updated app.js
    
    * created createNewsSchema for joi validation
    
    * emplemented body checking for createNews request
    
    * created getNewsList service
    
    * created getNews controller
    
    * emplemented get news request
    
    * created addReaction controller
    
    * emplemented addReaction request
    
    * commented google auth
    
    * created deleteNews controller
    
    * created delete news request
    
    * added checking if the news channel exists
    
    * made possibility to add user picture for registration
    
    * created get complexes controller
    
    * implemented get all cmplexes reguest
    
    * created findComplexById service
    
    * created getComplex controller
    
    * implemented get complex request
    
    * added users array to reaction
    
    * created 2 cases for reactions to reaction controller
    
    * added 3d case for reactions to reaction controller
    
    * added 4th case for reactions to reaction controller
    
    * implemented add reaction request
    
    * added image to createComplex controller
    
    * added image to create complex request
    
    * updated createComplex request for uploading image
    
    * implemented aploading several pictures to cloudinary
    
    * added removing images from tmp folder
    
    * updated comlex router
    
    * added images validation
    
    ---------
    
    Co-authored-by: VictorFil67 <victorfilinovych@gmail.com>
    Co-authored-by: VictorFil67 <142347802+VictorFil67@users.noreply.github.com>
    
    * fix: POST complexes/ with an image
    
    * Test /news
    
    ---------
    
    Co-authored-by: Kalmarr-dev <art.hydrogen1405@gmail.com>
    Co-authored-by: VictorFil67 <victorfilinovych@gmail.com>
    Co-authored-by: VictorFil67 <142347802+VictorFil67@users.noreply.github.com>

---
+++ tests/routers.spec.js
@@ -0,0 +1,386 @@
+import morgan from "morgan";
+import express from "express";
+import request from "supertest";
+import assert from 'assert';
+import mongoose from "mongoose";
+import { expect } from "chai";
+
+import authRouter from "../routes/authRouter.js";
+import usersRouter from "../routes/usersRouter.js";
+import complexesRouter from "../routes/complexesRouter.js";
+import chatRoomsRouter from "../routes/chatRoomsRouter.js";
+import notificationsRouter from "../routes/notificationsRouter.js";
+import votingsRouter from "../routes/votingsRouter.js";
+import newsChannelRouter from "../routes/newsChannelRouter.js";
+import newsRouter from "../routes/NewsRouter.js";
+import { readFileSync } from "fs";
+import path from "path";
+import { homedir } from "os";
+// import googleAuthRouter from "../routes/googleAuthRouter.js";
+
+const app = express();
+
+// app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.static("upload/images"));
+app.use("/auth", authRouter);
+app.use("/users", usersRouter);
+app.use("/complexes", complexesRouter);
+app.use("/chat_rooms", chatRoomsRouter);
+app.use("/notifications", notificationsRouter);
+app.use("/votings", votingsRouter);
+app.use("/news_channels", newsChannelRouter);
+app.use("/news", newsRouter);
+
+let bearerToken = null;
+const complexID = '67a3a0c37ca64083c1ff9799';
+let newsID = null;
+const newsChannelID = '6795115a5824a1b87515b14d';
+
+function getToken(done) {
+  request(app)
+    .post('/auth/login')
+    .send({email: 'Password1@email.com', password: "Password1"})
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function(err, res) {
+      if (err) {
+        throw err;
+      }
+      bearerToken = res.body.tokens.accessToken;          
+      done();
+    });
+}
+
+let userID = null;
+
+function getUserID(done) {
+  request(app)
+    .get('/auth/current')
+    .set("Authorization", "Bearer " + bearerToken)
+    .end(function(err, res) {
+      if (err) {
+        throw err;
+      }
+      userID = res.body._id;
+      done();
+    });
+}
+
+describe('Testing routes', () => {
+  before(function(done) {
+    this.timeout(10000);
+    mongoose
+      .connect(process.env.DB_HOST)
+      .then(() => {
+        done();
+      })
+  })
+
+  describe('Test /auth', () => {
+    beforeEach(getToken);
+
+    it("POST /login", function(done) {
+      request(app)
+        .post('/auth/login')
+        .send({email: 'Password1@email.com', password: "Password1"})
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            throw err;
+          }
+          done();
+        });
+    });
+
+    it("POST /logout", function(done) {
+      request(app)
+        .post('/auth/logout')
+        .send()
+        .set("Authorization", "Bearer " + bearerToken)
+        .expect(204)
+        .end(function(err, res) {          
+          if (err) {
+            throw err;
+          }
+          done();
+        });
+    });
+
+    it("GET /current", function(done) {
+      request(app)
+        .get('/auth/current')
+        .set("Authorization", "Bearer " + bearerToken)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          expect(res.body).to.have.all.keys("tokens", "_id", "name", "email",
+            "phone", "avatar", "is_admin", "buildings", "tempCode", "tempCodeTime");
+          if (err) {
+            throw err;
+          }
+          done();
+        });
+    });
+
+    it("GET /refresh", function(done) {
+      request(app)
+        .get('/auth/refresh')
+        .set("Authorization", "Bearer " + bearerToken)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          expect(res.body).to.have.all.keys("accessToken", "refreshToken");
+          if (err) {
+            throw err;
+          }
+          done();
+        });
+    });
+
+    it("PUT /forgot-password", function(done) {
+      request(app)
+        .put('/auth/forgot-password')
+        .send({ email: "Password1@email.com" })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            throw err;
+          }
+          done();
+        });
+    });
+
+    it("PUT /update", function(done) {
+      request(app)
+        .put('/auth/update')
+        .send({ email: "Password1@email.com", name: "password2@email.com" })
+        .set("Authorization", "Bearer " + bearerToken)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          expect(res.body.name).to.equal('password2@email.com');
+          if (err) {
+            throw err;
+          }
+          request(app)
+            .put('/auth/update')
+            .send({ email: "Password1@email.com", name: "password1@email.com" })
+            .set("Authorization", "Bearer " + bearerToken)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+              expect(res.body.name).to.equal('password1@email.com');
+              if (err) {
+                throw err;
+              }
+              done();
+            });
+        });
+    });
+  })
+
+  describe('Test /complexes', function() {
+    before(getToken);
+
+    it("POST /", function(done) {
+      this.timeout(10000);
+      request(app)
+        .post('/complexes')
+        .field('name', 'Test Complex')
+        .field('addresses', ['1', '2', '3a'])
+        .attach("image", path.join(homedir(), 'Downloads/Designer (7).jpeg'), 'utf8')
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(403)
+        .end(function(err, res) {          
+          if (err) {
+            throw err;
+          }
+          if (res.body._id) {
+            complexID = res.body._id;
+          }
+          
+          done();
+        });
+    });
+
+    it("PUT /:complexId", function(done) {      
+      request(app)
+        .put('/complexes' + '/' + complexID)
+        .send({
+          name: 'Test Complex', 
+          buildings: [ 
+            { address: '1', apartments: [{ number: 1, entrance: 1 }, { number: 2, entrance: 1 }] },
+            { address: '2', apartments: [{ number: 1, entrance: 1 }, { number: 2, entrance: 1 }] } 
+          ],
+        })
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(200)
+        .end(function(err, res) {          
+          if (err) {
+            throw err;
+          }
+          done();
+        });
+    });
+  })
+
+
+
+  describe('Test /users', function() {
+    before(getToken);
+    before(getUserID);
+
+    it("PUT /addresses", function(done) {
+      this.timeout(5000);
+      request(app)
+        .put('/users/addresses')
+        .send({
+          residential_complex: "Test Complex", building: "1", entrance: 1, apartment: 1,
+        })
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(200)
+        .end(function(err, res) {          
+          if (err) {
+            throw err;
+          }
+          request(app)
+            .put('/users/addresses')
+            .send({
+              residential_complex: "Test Complex", building: "1", entrance: 1, apartment: 2,
+            })
+            .set('Authorization', 'Bearer ' + bearerToken)
+            .expect(200)
+            .end(function(err, res) {          
+              if (err) {
+                throw err;
+              }
+              done();
+            });
+        });
+    });
+  
+    it("PATCH /addresses/:userID", function(done) {
+      request(app)
+        .patch('/users/addresses/' + userID)
+        .query({
+          residential_complex: "Test Complex", building: "1", entrance: 1, apartment: 2,
+        })
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(201)
+        .end(function(err, res) {          
+          if (err) {
+            throw err;
+          }
+          done();
+        });
+    });
+  
+    it("PATCH /addresses/:userID/:complexId", function(done) {
+      request(app)
+        .patch('/users/addresses/' + userID + '/' + complexID)
+        .query({
+          moderator: true
+        })
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(200)
+        .end(function(err, res) {          
+          if (err) {
+            throw err;
+          }
+          done();
+        });
+    });
+  
+    it("DELETE /addresses", function(done) {
+      request(app)
+        .delete('/users/addresses')
+        .send({
+          residential_complex: "Test Complex", building: "1", entrance: 1, apartment: 1,
+        })
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(200)
+        .end(function(err, res) {          
+          if (err) {
+            throw err;
+          }
+          request(app)
+            .delete('/users/addresses')
+            .send({
+              residential_complex: "Test Complex", building: "1", entrance: 1, apartment: 2,
+            })
+            .set('Authorization', 'Bearer ' + bearerToken)
+            .expect(200)
+            .end(function(err, res) {          
+              if (err) {
+                throw err;
+              }
+              done();
+            });
+        });
+    });
+  })
+
+  describe('Test /news', function() {
+    before(getToken);
+
+    it("POST /:newsChannelId", function(done) {
+      request(app)
+        .post('/news/' + newsChannelID)
+        .send({ picture: "Test",
+          title: "TEST NEWS",
+          titleUA: "ТЕСТОВА НОВИНА",
+          text: "Test news",
+          textUA: "Тестова новина"
+        })
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(201)
+        .end(function(err, res) {          
+          if (err) {
+            throw err;
+          }
+          newsID = res.body._id;
+          done();
+        });
+    });
+
+    it("PATCH /:newsId", function(done) {
+      request(app)
+        .patch('/news/' + newsID)
+        .send({ reaction: '&#x261D;' })
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(200)
+        .end(function(err, res) {          
+          if (err) {
+            throw err;
+          }
+          request(app)
+            .patch('/news/' + newsID)
+            .send({ reaction: '&#x261C;' })
+            .set('Authorization', 'Bearer ' + bearerToken)
+            .expect(200)
+            .end(function(err, res) {          
+              if (err) {
+                throw err;
+              }
+              done();
+            });
+        });
+    });
+
+    it("DELETE /:newsId", function(done) {
+      request(app)
+        .delete('/news/' + newsID)
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(200)
+        .end(function(err, res) {          
+          if (err) {
+            throw err;
+          }
+          done();
+        });
+    });
+  })
+})
