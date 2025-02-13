a89b5819b7665abc7492a1cb7cb2f471ad10d732
VictorFil67 <victorfilinovych@gmail.com>
2025-02-13 19:18:43 +0200

    added id validation for contact info requests

--- routes/contactInfoRouter.js
+++ routes/contactInfoRouter.js
@@ -3,6 +3,7 @@ import authenticate from "../middlewares/authenticate.js";
 import contactInfoControllers from "../controllers/contactInfoControllers.js";
 import validateBody from "../decorators/validateBody.js";
 import { createContactInfoSchema } from "../schemas/contactInfoSchemas.js";
+import isValidId from "../middlewares/isValidId.js";
 
 const contactInfoRouter = Router();
 
@@ -10,11 +11,13 @@ contactInfoRouter.use(authenticate);
 
 contactInfoRouter.post(
   "/:residential_complex_id/:building_id?",
+  isValidId,
   validateBody(createContactInfoSchema),
   contactInfoControllers.createContactInfo
 );
 contactInfoRouter.delete(
   "/:contactInfoId",
+  isValidId,
   contactInfoControllers.deleteContactInfo
 );
 
