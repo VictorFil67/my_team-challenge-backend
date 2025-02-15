140fd553aaa4c2f5e5e179b6a722f080c3ad5fe3
VictorFil67 <victorfilinovych@gmail.com>
2025-02-15 23:08:21 +0200

    created check access in base of updateContactInfo controller

--- controllers/contactInfoControllers.js
+++ controllers/contactInfoControllers.js
@@ -65,6 +65,9 @@ const deleteContactInfo = async (req, res) => {
   const { is_admin, buildings } = req.user;
 
   const contactInfo = await findContactInfoById(_id);
+  if (!contactInfo) {
+    throw HttpError(404, "Contact info not found");
+  }
 
   const residential_complex_id = contactInfo.residential_complex_id;
 
@@ -87,6 +90,37 @@ const deleteContactInfo = async (req, res) => {
 
   const result = await removeContactInfo(_id);
   res.json(result);
+};
+
+const updateContactInfo = async (req, res) => {
+  const { contactInfoId: _id } = req.params;
+  const { is_admin, buildings } = req.user;
+
+  const contactInfo = await findContactInfoById(_id);
+  if (!contactInfo) {
+    throw HttpError(404, "Contact info not found");
+  }
+
+  const residential_complex_id = contactInfo.residential_complex_id;
+
+  const searchComplex = buildings.find((elem) => {
+    return (
+      elem.residential_complex_id.toString() ===
+      residential_complex_id.toString()
+    );
+  });
+
+  if (!is_admin && !searchComplex) {
+    throw HttpError(403, `The user is not related to the specified complex.`);
+  }
+
+  const moderator = is_admin ? false : searchComplex.moderator;
+  console.log(moderator);
+  if (!is_admin && !moderator) {
+    throw HttpError(403, "You don't have access to this action!");
+  }
+
+  res.json("OK!!!!!!!!!!!!!");
 };
 
 export default {
